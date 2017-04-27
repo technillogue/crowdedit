fcc3641a34be4d48b685678daa162bbcb4791a3e
enbug <enbug@example.com>
2017-04-27 20:11:42 +0000

    App is slightly usable for its purpose now! Yay!

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, session, redirect, render_template, request, url_for
 from sqlalchemy import *
+import sqlalchemy.sql
+import sqlalchemy
 #from flask_sqlalchemy import SQLAlchemy
 import datetime, sys
 from random import choice
@@ -19,6 +21,7 @@ votes = Table("votes", metadata,
     Column("id", Integer, primary_key=True),
     Column("snippet_id", Integer, nullable=False, index=True),
     Column("positive", Boolean, nullable=False, index=True),
+    Column("votername", String(128), index=True),
     Column("created_at", Date, default=datetime.datetime.now),
     Column("updated_at", Date, onupdate=datetime.datetime.now),
     Column("approximate_user_identifier", String(128))
@@ -34,34 +37,45 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
 #app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
-#metadata.create_all(engine)
+metadata.create_all(engine)
 
 #db = SQLAlchemy(app)
 #from flask_app import db
 #db.create_all()
 #conn = engine.connect()
 
-
+app.secret_key = 'gbxtqmypswnohyatyulm'
 
 #todo:
 # - only show undownvoted snippets
 # - don't show the same snippet twice to the same person
 # - ask for a name to sign the votes with
 
+@app.route("/name", methods=["POST"])
+def name():
+    session['name'] = request.form["name"]
+    return redirect(url_for('index'))
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if 'name' not in session:
+        return render_template("name.html")
     conn = engine.connect()
     if request.method == "GET":
-        sql = '''SELECT * FROM snippets WHERE NOT EXISTS
-            (SELECT * FROM votes WHERE snippet_id = snippets.id AND positive=0);'''
-        # NOTE if it's too slow, this line can be improved:
-        snippet = choice(list(conn.execute(sql)))
+        query = sqlalchemy.sql.text('''SELECT * FROM snippets WHERE NOT EXISTS
+            (SELECT * FROM votes WHERE
+               (votes.snippet_id = snippets.id
+               AND (votes.positive=0 OR votes.votername = :name)));''')
+        snippets_to_vote_on = list(conn.execute(query, name=session['name']))
         conn.close()
+        if len(snippets_to_vote_on) == 0:
+            return '<p>Congratulations you are done!</p>'
+        snippet = choice(snippets_to_vote_on)
         return render_template("main.html", passage=snippet.text, id=snippet.id)
     vote = (request.form["vote"] == "True")
     snippet_id = request.form["snippet_id"]
     sys.stderr.write("hi {} {}".format(vote, snippet_id))
-    ins = votes.insert().values(snippet_id=snippet_id, positive=vote)
+    ins = votes.insert().values(snippet_id=snippet_id, positive=vote, votername = session['name'])
     conn.execute(ins)
     conn.close()
     return redirect(url_for('index'))
