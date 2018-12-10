524277a410d7f43c405a4d4890d225aad58c7357
enbug <enbug@example.com>
2018-12-10 16:25:13 +0000

    moved to git

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 from pprint import pprint as pp
 import pdb
@@ -17,6 +16,9 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 admin = Admin(app, name='enbugstuff', template_mode='bootstrap3', url='/0d20f554eb0bf160df9ef424a1a1b436')
 
+index_of_last_edited_snippet_in_order_of_rank = 53
+#btw = best_to_worst(c("select * from snippets"), engine, True)
+#btw[index...]
 
 # SQL stuff:
 metadata = MetaData()
@@ -79,23 +81,37 @@ def get_weights():
         votes = Counter(v[0] for v in conn.execute("select votername from votes"))
         weights = {
             voter:
-                (1.5 if voter in pubs else 1.0) *
-                (2 ** (votes[voter]/90)) * 0.5
+                (.4 if voter == "Klara Du Plessis" else -.1) +
+                (.4 if voter in pubs else -0.1) +
+                1.8 / (1 + 2**-((votes[voter] - 12)/14))  # ranges from 0.4 to 1.6, halfway point of 1 at 28 votes
             for voter in votes.keys()
             }
         return weights
     finally:
         conn.close()
 
-def get_rank(snippet, conn, weights):
+def get_rank(snippet, conn, weights, return_weight=False):
     votes_here = conn.execute(sqlalchemy.sql.text('''select * from votes where snippet_id=:id'''), id=snippet.id)
-    rank = sum(weights[vote.votername]*{0:-1,1:1}[vote.positive] for vote in votes_here)
-    return rank
+    total_weight = 0
+    total_positive = 0
+    for vote in votes_here:
+        weight = weights[vote.votername]
+        total_positive += {0:-1, 1:1}[vote.positive] * weight
+        total_weight += weight
+    try:
+        rank = total_positive / (total_weight)**0.1
+        # this isn't quite a weighted average -- of snippets where 100% of
+        # people voted for them, those where more people voted should have
+        # higher rankings than those where less people voted. however, this
+        # should be scaled down... I might be completely wrong.
+    except ZeroDivisionError:
+        rank = 0
+    return (rank, total_weight) if return_weight else rank
 
 def best_to_worst(snippets, conn, ranked=False):
     weights = get_weights()
     ranks = sorted([
-        (get_rank(snippet, conn, weights), snippet)
+        (get_rank(snippet, conn, weights, ranked), snippet)
         for snippet in snippets
     ], reverse=True)
     if ranked:
@@ -110,28 +126,31 @@ def index():
     conn = engine.connect()
     try:
         if request.method == "GET":
-            if request.args.get('mode') == 'up':
-                bad_votes_condition = 'votes.positive=0'
-            else:
-                bad_votes_condition = '0=1'
+            mode = request.args.get("mode", "positive")
+            order = request.args.get("order", "least_weight")
+            modes = {"positive": "OR votes.positive=0", "all": ""}
             query = sqlalchemy.sql.text('''SELECT * FROM snippets
             WHERE NOT EXISTS
             (SELECT * FROM votes WHERE
                    (votes.snippet_id = snippets.id
-                   AND ('''+bad_votes_condition+''' OR votes.votername = :name)));
+                   AND (votes.votername = :name ''' + modes[mode] + ''')));
             ''')  # where not voted-upon
              #where exists vote.positive="+ "0" if get_best(wc_only=True) > 10000 else "1"+" and vote.id...
             snippets_to_vote_on = list(conn.execute(query, name=session['name']))
             if len(snippets_to_vote_on) == 0:
                 return '<p>you have voted on everything!</p>'
             weights = get_weights()
-            snippets_to_vote_on.sort(
-                key = lambda snip:sum(
-                    weights[vote.votername] for vote in
-                    conn.execute(sqlalchemy.sql.text("select * from votes where snippet_id=:id"),id=snip.id)
+            if order == "least_weight":
+                snippets_to_vote_on.sort(
+                    key = lambda snip:sum(
+                        weights[vote.votername] for vote in
+                        conn.execute(sqlalchemy.sql.text("select * from votes where snippet_id=:id"),id=snip.id)
+                    ) #probably better to refactor this into get_rank
                 )
-            )
+            elif order == "random":
+                shuffle(snippets_to_vote_on)
             snippet = snippets_to_vote_on[0]
+            # this is could be refactored away; best_to_worst can do this
 
 
             ##  stats
@@ -208,12 +227,13 @@ def addsnippet():
     conn = engine.connect()
     try:
         for section in request.form["text"].split("%["):
-            title, section = section.split("\n", 1)
-            sys.stderr.write(title)
-            for snippet in section.split("%split%"):
-                snippet = snippet.strip() + "\n[day " + title + "]"
-                ins = snippets.insert().values(text = snippet)
-                conn.execute(ins)
+            if section:
+                title, section = section.split("\n", 1)
+                sys.stderr.write(title)
+                for snippet in section.split("%split%"):
+                    snippet = snippet.strip() + "\n[day " + title + "]"
+                    ins = snippets.insert().values(text = snippet)
+                    conn.execute(ins)
     finally:
         conn.close()
     return redirect(url_for('index'))
@@ -285,10 +305,11 @@ def hierarchize():
     finally:
         conn.close()
 
-def get_best(threshold=1):
+def get_best(threshold=1, scores=False, goal_wc=10000):
     conn  = engine.connect()
     try:
-        snippets = list(conn.execute('''
+        snippets = list(conn.execute("SELECT id, text FROM snippets"))
+        '''
                 SELECT
                 snippets.id AS id,
                 snippets.text AS text,
@@ -297,27 +318,28 @@ def get_best(threshold=1):
                 (SELECT COUNT(*) FROM votes WHERE snippets.id = votes.snippet_id AND votes.positive = 0) AS downvotecount
                 FROM snippets
                 ORDER BY (upvotecount - downvotecount) ASC
-                ;''')) # most of it is redundant
+                ;'''# most of it is redundant
 
-        def acceptable(snip):
-            return (snip.upvotecount - snip.downvotecount) > (0.8 + float(threshold))
-        snippets = list(filter(acceptable, reversed(best_to_worst(snippets, conn))))
+        snippets = list(
+            filter(
+                lambda snippet:snippet[0][0] > float(threshold),
+                reversed(best_to_worst(snippets, conn, True))
+            )
+        ) # worst to best, for efficient popping
         output = []
         wc = 0
         early = []
         end = []
         late = []
-        while wc < 10000 and len(snippets):
-            snippet = snippets.pop()
-            if int(snippet.id) == 3579:
-                end.append(snippet.text)
-            elif int(snippet.id) == 3580:
-                late.append(snippet.text)
-            elif int(snippet.id) in (2855, 2847):
-                early.append(snippet.text)
-            else:
-                output.append(snippet.text)
+        while wc < int(goal_wc) and len(snippets):
+            rank, snippet = snippets.pop()
             wc += len(snippet.text.split())
+            dest = {3579:end, 3580:late, 2855: early, 2847: early}
+            dest.get(snippet.id, output).append(
+                snippet.text + "\n[score: {:.3f}/{:.3f}]".format(rank[0], rank[1]) \
+                if scores else snippet.text
+
+            )
         # randomize order
         output.sort(key=lambda x:len(x))
         mid = len(output)//2
@@ -347,17 +369,20 @@ def get_best(threshold=1):
 @app.route("/best")
 def best():
     threshold =request.args.get("threshold", 0)
-    output = get_best(threshold)
+    scores=request.args.get("scores", False)
+    wc=request.args.get("wc", 10000)
+    output = get_best(threshold, scores, wc)
     return "<html><body><div style=\"margin:auto;width:600px\"><p>" + "</p><p>".join(output).replace("\n", " <br/> ") + "</p></div></body><html>"
 
 
 
 @app.route("/admin")
 def admin():
-    query = "SELECT * FROM snippets JOIN votes ON snippets.id = votes.snippet_id"
+    table = request.args.get("table")
+    query = "SELECT * FROM snippets JOIN votes ON snippets.id = votes.snippet_id ORDER BY votes.created_at"
     conn = engine.connect()
     try:
-        voterstats = []
+        voterstats = ["name;\t\tweight;\t\tvotes;\t\tpositivity;\t\tcomments"] if table else []
         weights = get_weights()
         for voter, weight in sorted(weights.items(), key=lambda x:-x[1]):
             yourvotecount = Counter(
@@ -366,18 +391,21 @@ def admin():
                             "SELECT positive FROM votes WHERE votername = :name"),
                             name=voter)
                     )
-
-            voterstats.append("{} - weight {} - has commented {} times and voted {} times, {}% positivty".format(
-                voter if voter else "adam zachery",
-                weight,
-                list(conn.execute(sqlalchemy.sql.text("select count(*) from comments where votername = :name"), name=voter))[0][0],
-                sum(yourvotecount.values()),
-                100*float(yourvotecount[1])/sum(yourvotecount.values()) if yourvotecount else "n/a"
-            ))
+            voterstats.append(
+                ((lambda *x:";\t\t".join(x)) if table else ("{} - weight {} - has voted {} times, {}% positivty, and commented {} times".format))(
+                *map(str, (
+                    voter if voter else "adam zachery",
+                    weight,
+                    sum(yourvotecount.values()),
+                    100*float(yourvotecount[1])/sum(yourvotecount.values()) if yourvotecount else "n/a",
+                    list(conn.execute(sqlalchemy.sql.text("select count(*) from comments where votername = :name"), name=voter))[0][0]
+                ))
+                )
+            )
         votes = conn.execute(query)
 
         comments = list(conn.execute("""SELECT snippets.text AS passage, comments.text AS comment, comments.votername AS author FROM
-                    snippets JOIN comments ON snippets.id = comments.snippet_id"""))
+                    snippets JOIN comments ON snippets.id = comments.snippet_id ORDER BY comments.created_at"""))
     finally:
         conn.close()
     return render_template("admin.html", voterstats=voterstats, votes=votes, comments=comments)
