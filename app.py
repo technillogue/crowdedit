9e08bc54cdcb3cfeb79e3aa42b3ecbb8e44b1460
Sylvie <wisepoison@gmail.com>
2019-01-11 23:54:50 +0000

    removed old import/export/update and fixed style

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 import datetime
-import sys
-import re
-import json
-from collections import Counter
 from random import shuffle, randint
-from flask import Flask, session, redirect, render_template, request, url_for, Response
+from flask import Flask, session, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import and_, or_, not_, exists, alias, case, func, cast
+from sqlalchemy import and_, exists, alias, case, func, cast
 
 
 # for updating activity_count after manually changing votes or comments:
-#UPDATE public.user SET activity_count = (select count(*) from vote where vote.user_id = "user".id) + (select count(*) from comment where comment.user_id="user".id);
+#UPDATE public.user SET activity_count = (select count(*) FROM vote
+# WHERE vote.user_id = "user".id) + (select count(*) FROM comment
+# WHERE comment.user_id="user".id);
 
 # TODOs:
-# - finish output
 # - export/import
-# - admin
-# - editing (with edited_at)
+# - editing (including edited_at)
 # - alter weights to favor less extreme voters
 
 IMPORTANT_NAMES = [
@@ -43,7 +39,8 @@ app.config["DEBUG"] = True
 if app.config["DEBUG"]:
     from pprint import pprint as pp
 
-SQLALCHEMY_DATABASE_URI = "postgres+psycopg2://{username}:{password}@{hostname}/{databasename}".format(
+SQLALCHEMY_DATABASE_URI = \
+"postgres+psycopg2://{username}:{password}@{hostname}/{databasename}".format(
     username="enbug",
     hostname="localhost",
     password="local",
@@ -58,7 +55,7 @@ db = SQLAlchemy(app)
 
 class Snippet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text =  db.Column(db.Text, nullable=False)
+    text = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return "<Snippet {} '{}'>".format(
@@ -230,7 +227,8 @@ def index():
         # the most common version of this query goes
         #SELECT snippet.id
         #FROM snippet
-        #LEFT OUTER JOIN vote AS personal on personal.snippet_id = snippet.id AND personal.user_id=1
+        #LEFT OUTER JOIN vote
+        #AS personal on personal.snippet_id = snippet.id AND personal.user_id=1
         #JOIN vote ON vote.snippet_id = snippet.id
         #WHERE personal.id IS NULL
         #GROUP BY snippet.id
@@ -255,15 +253,14 @@ def index():
         if app.config["DEBUG"]:
             expected_snippets = (
                 Snippet.query.count()
-                - Vote.query.filter(Vote.user_id==current_user.id).count()
+                - Vote.query.filter(Vote.user_id == current_user.id).count()
             )
             actual_snippets = query.count()
             app.logger.info("expected {}, actually {}".format(
                 expected_snippets,
                 actual_snippets
             ))
-            assert (actual_snippets == expected_snippets)
-
+            assert actual_snippets == expected_snippets
         snippet = query.first()
         if snippet is None:
             return "<p> you have voted on every snippet! </p>"
@@ -276,37 +273,37 @@ def index():
             )
         )
         average_activity = User.query.with_entities(
-                func.sum(User.activity_count)
-            ).scalar() / float(User.query.count())
+            func.sum(User.activity_count)
+        ).scalar() / float(User.query.count())
         yep_count = Vote.query.filter(
-                Vote.user_id==current_user.id,
-                Vote.valence==True
-            ).count()
+            Vote.user_id == current_user.id,
+            Vote.valence
+        ).count()
         nop_count = Vote.query.filter(
-                Vote.user_id==current_user.id,
-                Vote.valence==False
-            ).count()
+            Vote.user_id == current_user.id,
+            not Vote.valence
+        ).count()
         if nop_count:
             positivity = int(100 * yep_count / float(yep_count + nop_count))
         else:
             positivity = "n/a"
         stats = [
             "you, user #{}, {}, have voted and commented {} times".format(
-                    current_user.id,
-                    current_user.name,
-                    current_user.activity_count
-                ),
+                current_user.id,
+                current_user.name,
+                current_user.activity_count
+            ),
             "the average user has voted and commented {} times".format(
-                    average_activity
-                ),
+                average_activity
+            ),
             "you have voted {} yep and {} nop, {}% positivity".format(
-                    yep_count,
-                    nop_count,
-                    positivity
-                ),
+                yep_count,
+                nop_count,
+                positivity
+            ),
             "{} snippets that you can vote on remain".format(
-                    query.count()
-                ),
+                query.count()
+            ),
         ]
         return render_template(
             "main.html",
@@ -351,6 +348,8 @@ def best():
     special_placement = {"start": [], "early": [], "late": [], "end": []}
     while wordcount < wordcount_goal and len(snippets):
         snippet = snippets.pop()
+        if snippet.score < score_threshold:
+            break
         wordcount += len(snippet.text.split())
         if show_scores:
             text = "{}\n[score: {:.3f}/{:.3f}]".format(
@@ -370,7 +369,7 @@ def best():
     middle = int(len(selected_snippets)/2)
     sorted_by_length = sorted(
         selected_snippets,
-        key = lambda snippet: len(snippet)
+        key=len
     )
     middle_length = len(sorted_by_length[middle])
     shorter = sorted_by_length[:middle]
@@ -411,9 +410,38 @@ def best():
     return render_template("best.html", snippets=output)
 
 
-@app.route("/new_admin")
-def new_admin():
-    organize_by = request.args.get("organize_by", "time") # or "snippet"
+# SQL for the user_stats
+#SELECT
+    #public.user.id,
+    #public.user.name,
+    #public.user.activity_count,
+    #pos.votes/CAST(count(everything.id) as float) AS positivity
+#FROM
+    #public.user
+#LEFT JOIN
+    #(SELECT
+        #user_id, COUNT(*) AS votes
+    #FROM
+        #vote
+    #WHERE
+        #valence=true
+    #GROUP BY
+        #user_id) AS pos
+#ON
+    #public.user.id = pos.user_id
+#LEFT JOIN
+    #vote as everything
+#ON
+    #public.user.id=everything.user_id
+#GROUP BY
+    #public.user.id, pos.votes
+#ORDER BY
+#public.user.id;
+
+
+@app.route("/admin")
+def admin():
+    #organize_by = request.args.get("organize_by", "time") # or "snippet"
     positive_vote = Vote.query.with_entities(
         Vote.user_id,
         func.count(Vote.id).label("vote_count")
@@ -433,7 +461,7 @@ def new_admin():
         ).label("positivity")
     ).outerjoin(
         positive_vote,
-        positive_vote.c.user_id==User.id
+        positive_vote.c.user_id == User.id
     ).outerjoin(
         all_vote,
         all_vote.c.user_id == User.id
@@ -445,173 +473,19 @@ def new_admin():
         snippets.append({
             "text": snippet.text,
             "votes": Vote.query.with_entities(
-                    Vote.user_id,
-                    User.name.label("user_name"),
-                    Vote.valence
-                ).join(User).filter(Vote.snippet_id == snippet.id).all(),
+                Vote.user_id,
+                User.name.label("user_name"),
+                Vote.valence
+            ).join(User).filter(Vote.snippet_id == snippet.id).all(),
             "comments": Comment.query.with_entities(
-                    Comment.user_id,
-                    User.name.label("user_name"),
-                    Comment.text
-                ).join(User).filter(Vote.snippet_id == snippet.id).all()
+                Comment.user_id,
+                User.name.label("user_name"),
+                Comment.text
+            ).join(User).filter(Vote.snippet_id == snippet.id).all()
         })
 
     return render_template(
         "admin.html",
-        user_stats = user_stats,
-        snippets = snippets
+        user_stats=user_stats,
+        snippets=snippets
     )
-
-
-"""
-SELECT
-    public.user.id,
-    public.user.name,
-    public.user.activity_count,
-    pos.votes/CAST(count(everything.id) as float) AS positivity
-FROM
-    public.user
-LEFT JOIN
-    (SELECT
-        user_id, COUNT(*) AS votes
-    FROM
-        vote
-    WHERE
-        valence=true
-    GROUP BY
-        user_id) AS pos
-ON
-    public.user.id = pos.user_id
-LEFT JOIN
-    vote as everything
-ON
-    public.user.id=everything.user_id
-GROUP BY
-    public.user.id, pos.votes
-ORDER BY
-public.user.id;
-"""
-
-
-@app.route("/admin")
-def admin():
-    table = request.args.get("table")
-    query = "SELECT * FROM snippets JOIN votes ON snippets.id = votes.snippet_id ORDER BY votes.created_at"
-    conn = engine.connect()
-    try:
-        voterstats = ["name;\t\tweight;\t\tvotes;\t\tpositivity;\t\tcomments"] if table else []
-        weights = get_weights()
-        for voter, weight in sorted(weights.items(), key=lambda x:-x[1]):
-            yourvotecount = Counter(
-                item[0] for item in conn.execute(
-                    sqlalchemy.sql.text(
-                            "SELECT valence FROM votes WHERE user = :name"),
-                            name=voter)
-                    )
-            voterstats.append(
-                ((lambda *x:";\t\t".join(x)) if table else ("{} - weight {} - has voted {} times, {}% positivty, and commented {} times".format))(
-                *map(str, (
-                    voter if voter else "adam zachery",
-                    weight,
-                    sum(yourvotecount.values()),
-                    100*float(yourvotecount[1])/sum(yourvotecount.values()) if yourvotecount else "n/a",
-                    list(conn.execute(sqlalchemy.sql.text(u"select count(*) from comments where user = :name"), name=voter.encode("utf-8")))[0][0]
-                ))
-                )
-            )
-        votes = conn.execute(query)
-        comments = list(conn.execute("""SELECT snippets.text AS passage, comments.text AS comment, comments.user AS author FROM
-                    snippets JOIN comments ON snippets.id = comments.snippet_id ORDER BY comments.created_at"""))
-    finally:
-        conn.close()
-    return render_template("admin.html", voterstats=voterstats, votes=votes, comments=comments)
-
-
-
-
-
-
-
-
-@app.route("/updatesnippet", methods=["GET", "POST"])
-def updatesnippet():
-    conn = engine.connect()
-    try:
-        if request.method == "GET":
-            snippets = conn.execute('''
-            SELECT
-            snippets.id AS id,
-            snippets.text AS text,
-            (SELECT COUNT(*) FROM votes WHERE snippets.id = votes.snippet_id) AS votecount,
-            (SELECT COUNT(*) FROM votes WHERE snippets.id = votes.snippet_id AND votes.valence = True) AS upvotecount,
-            (SELECT COUNT(*) FROM votes WHERE snippets.id = votes.snippet_id AND votes.valence = False) AS downvotecount,
-            (SELECT COUNT(*) FROM comments WHERE snippets.id = comments.snippet_id) AS commentcount
-            FROM snippets
-            ORDER BY (upvotecount + commentcount - downvotecount) DESC
-            ;''')
-            return render_template("updatesnippet.html", snippets = snippets)
-        snippetid = int(request.form["id"])
-        newtext = request.form["text"]
-        conn.execute(sqlalchemy.sql.text(
-                            "UPDATE snippets SET text=:text WHERE id=:id"), text=newtext, id=snippetid)
-    finally:
-        conn.close()
-    return redirect(url_for('updatesnippet'))
-
-
-@app.route("/downloadjson", methods=["GET"])
-def downloadjson():
-    return Response(jsonize(), mimetype='application/json; charset=utf-8')
-
-
-@app.route("/addjson", methods=["GET", "POST"])
-def addjson():
-    if request.method == "GET":
-        return render_template("uploadjson.html")
-    new_snippets = json.loads(request.form["json"])
-    conn = engine.connect()
-    try:
-        for snippet_id, snippet_text, new_votes, new_comments in new_snippets:
-            ins = snippets.insert().values(text=snippet_text)
-            sippet_id = conn.execute(ins).inserted_primary_key[0]
-            for old_id, user, valence in new_votes:
-                conn.execute(votes.insert().values(
-                    snippet_id=snippet_id,
-                    valence = valence,
-                    user = user
-                ))
-            for old_id, user, comment_text in new_comments:
-                conn.execute(comments.insert().values(
-                    snippet_id=snippet_id,
-                    text = comment_text,
-                    user = user
-                ))
-        return redirect(url_for('addjson') + '?were_updated_count=' + str(len(new_snippets)))
-    finally:
-        conn.close()
-
-#@app.route("/uploadjson", methods=["GET", "POST"])
-#def uploadjson():
-#    if request.method == "GET":
-#        return render_template("uploadjson.html")
-#    snippets = json.loads(request.form["json"])
-#    conn = engine.connect()
-#    try:
-#        updatecount = 0
-#        for snippet_id, snippet_text, votes, comments in snippets:
-#            snippet_id = newthing[0]
-#            updatecount += conn.execute(
-#                    sqlalchemy.sql.text(
-#                            "UPDATE snippets SET text=:newtext WHERE id=:id AND text != :newtext"),
-#                        id=snippet_id, newtext=snippet_text
-#                    ).rowcount
-#        return redirect(url_for('') + '?were_updated_count=' + str(updatecount))
-#    finally:
-#        conn.close()
-
-"""
-takes [[snippet_id, snippet_text, [[vote_id, user, vote_valence],
-...], [comment_id, user, comment], ...]
-returns ([snippets], [votes], [comments]
-"""
-
